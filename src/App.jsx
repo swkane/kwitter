2931483dd336d4ae41902964b46f78c9cd54091b
swkane <samuelwkane@gmail.com>
2018-06-19 15:40:39 -0400

    added basic dependencies

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>We are Kwitters, bum, babum, bumbum, bumbum.</h1>
+    );
+  }
+}
+
+export default App;
