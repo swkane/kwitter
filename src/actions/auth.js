2c7086b6732fe0ac98ec5c5fe182e6ed90e01671
swkane <samuelwkane@gmail.com>
2018-06-19 18:13:02 -0400

    rough actions and reducer for register and login. in desperate need to test

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -11,5 +11,43 @@ export const setToken = token => ({
 // ASYNC
 
 export const asyncRegisterUser = ({ username, password, displayName }) => dispatch => {
+    fetch( 'https://kwitter-api.herokuapp.com/auth/register', {
+        method: "POST",
+        headers: new Headers( {
+          "Content-Type": "application/json",
+        } ),
+        body: JSON.stringify( { username, password, displayName } ),
+      } )
+    .then( ( response ) => {
+        if ( response.ok ) {
+          return response.json();
+        }    
+      } )
+    .then( response => console.log(response))
+    .catch( ( error ) => {
+        console.error(error);
+      } );
+    
+};
+
+export const asyncLogin = ({ username, password }) => dispatch => {
+    fetch( 'https://kwitter-api.herokuapp.com/auth/login', {
+        method: "POST",
+        headers: new Headers( {
+          "Content-Type": "application/json",
+        } ),
+        body: JSON.stringify( { username, password } ),
+      } )
+    .then( ( response ) => {
+        if ( response.ok ) {
+          return response.json();
+        }    
+      } )
+    .then( ({ token }) => {
+        dispatch(setToken(token));
+    })
+    .catch( ( error ) => {
+        console.error(error);
+      } );
     
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import { token } from './auth';
 
-export default combineReducers({})
+export default combineReducers({
+    token
+})
