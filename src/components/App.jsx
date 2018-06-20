6ad93306327afd6b8ced3cebc38ddbafa6ae6de1
swkane <samuelwkane@gmail.com>
2018-06-20 09:54:24 -0400

    commented out test component for use later ;)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import TestComponent from './TestComponent';
+// import TestComponent from './TestComponent';
+
+// I use the testcomponent to quickly test redux actions etc
 
 class App extends Component {
   render() {
 
     return (
-      // <h1>We are Kwitters, bum, babum, bumbum, bumbum.</h1>
-      <TestComponent />
+      <h1>We are Kwitters, bum, babum, bumbum, bumbum.</h1>
+      // <TestComponent />
     );
   }
 }
