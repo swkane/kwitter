a4d2d24494805ace47a8a195232bb6d5fb951fd7
Declan Vea <declanpvea@gmail.com>
2018-06-25 16:42:06 -0400

    added segments to ui

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {Container, Form, Button, Feed, Icon} from 'semantic-ui-react'
+import {Container, Form, Button, Feed, Icon, Segment} from 'semantic-ui-react'
 import { addMessage, addLike } from '../actions/messages';
 import {image} from '../../src/semanticAssets';
 
@@ -36,31 +36,33 @@ class Messages extends React.Component {
             <div>
                 <Container>
                     <Form>
-                        <Form.TextArea rows="5" width="7" maxLength="150" value={this.state.message} onChange={this.handleMessage} />
+                        <Form.TextArea rows="5" maxLength="150" value={this.state.message} onChange={this.handleMessage} />
                         <div>You have {count} characters remaining</div>
                         <Button color="blue" onClick={this.submitMessage}>Share</Button>
                     </Form>
                     { messages.map( (message, i) => (
-                        <Feed key={i} size="large">
-                            <Feed.Event>
-                                <Feed.Label>
-                                    <img src={image} alt="image" />
-                                </Feed.Label>
-                                <Feed.Content>
-                                    <Feed.Summary>
-                                        <Feed.User>{message.id}</Feed.User>
-                                        <Feed.Date>{message.updatedAt}</Feed.Date>
-                                    </Feed.Summary>
-                                    <Feed.Extra text>{message.text}</Feed.Extra>
-                                    <Feed.Meta>
-                                        <Feed.Like>
-                                            <Icon onClick={this.handleLikes} name="like" />
-                                            {message.likes.length}
-                                        </Feed.Like>
-                                    </Feed.Meta>
-                                </Feed.Content>
-                            </Feed.Event>
-                        </Feed>
+                        <Segment>
+                            <Feed key={i} size="large">
+                                <Feed.Event>
+                                    <Feed.Label>
+                                        <img src={image} alt="image" />
+                                    </Feed.Label>
+                                    <Feed.Content>
+                                        <Feed.Summary>
+                                            <Feed.User>{message.id}</Feed.User>
+                                            <Feed.Date>{message.updatedAt}</Feed.Date>
+                                        </Feed.Summary>
+                                        <Feed.Extra text>{message.text}</Feed.Extra>
+                                        <Feed.Meta>
+                                            <Feed.Like>
+                                                <Icon onClick={this.handleLikes} name="like" />
+                                                {message.likes.length}
+                                            </Feed.Like>
+                                        </Feed.Meta>
+                                    </Feed.Content>
+                                </Feed.Event>
+                            </Feed>
+                        </Segment>
                     ))}
                 </Container>
             </div>
